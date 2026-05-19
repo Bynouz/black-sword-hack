90997625d0e707c9f9c2f418d9d08bfd890264ad
Bynouz <74014467+Bynouz@users.noreply.github.com>
2026-05-19 20:33:48 +0200

    Update spirit-sheet.js

--- modules/sheets/spirit-sheet.js
+++ modules/sheets/spirit-sheet.js
@@ -1,11 +1,24 @@
-export default class SpiritSheet  extends ItemSheet {
-	get template() {
-		return("systems/black-sword-hack/templates/sheets/spirit-sheet.html");
-	}
+export default class SpiritSheet extends ItemSheet {
 
-	getData() {
-		let data = super.getData();
-		data.configuration = CONFIG.configuration;
-		return(data);
-	}
+    static get defaultOptions() {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+            classes:  ["bsh", "bsh-sheet", "bsh-spirit-sheet", "sheet"],
+            height:   400,
+            width:    550,
+            template: "systems/black-sword-hack/templates/sheets/spirit-sheet.html"
+        });
+    }
+
+    get template() {
+        return "systems/black-sword-hack/templates/sheets/spirit-sheet.html";
+    }
+
+    // V14 : getData() retourne item et item.system directement
+    getData() {
+        const context         = super.getData();
+        context.configuration = CONFIG.configuration;
+        context.item          = this.item;
+        context.system        = this.item.system;
+        return context;
+    }
 }
