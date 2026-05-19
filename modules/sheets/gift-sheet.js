16728c2f750d4a3f729a4897d56b75ce4f3171ef
Bynouz <74014467+Bynouz@users.noreply.github.com>
2026-05-19 20:29:00 +0200

    Update gift-sheet.js

--- modules/sheets/gift-sheet.js
+++ modules/sheets/gift-sheet.js
@@ -1,20 +1,24 @@
-export default class GiftSheet  extends ItemSheet {
+export default class GiftSheet extends ItemSheet {
+
     static get defaultOptions() {
-        return(foundry.utils.mergeObject(super.defaultOptions,
-                                         {classes:  ["bsh", "bsh-sheet", "bsh-gift-sheet", "sheet"],
-                                          height:   450,
-                                          template: "systems/bsh/templates/sheets/gift-sheet.html",
-                                          width:    600}));
+        return foundry.utils.mergeObject(super.defaultOptions, {
+            classes:  ["bsh", "bsh-sheet", "bsh-gift-sheet", "sheet"],
+            height:   450,
+            width:    600,
+            template: "systems/black-sword-hack/templates/sheets/gift-sheet.html"
+        });
     }
 
-	get template() {
-		return("systems/black-sword-hack/templates/sheets/gift-sheet.html");
-	}
+    get template() {
+        return "systems/black-sword-hack/templates/sheets/gift-sheet.html";
+    }
 
-	getData() {
-		let context = super.getData();
-
-		context.configuration = CONFIG.configuration;
-		return(context);
-	}
+    // V14 : getData() retourne item et item.system directement
+    getData() {
+        const context         = super.getData();
+        context.configuration = CONFIG.configuration;
+        context.item          = this.item;
+        context.system        = this.item.system;
+        return context;
+    }
 }
