55330932bf240c0f2dc4140d5e95667e268dd19e
Bynouz <74014467+Bynouz@users.noreply.github.com>
2026-05-19 20:28:43 +0200

    Update demon-sheet.js

--- modules/sheets/demon-sheet.js
+++ modules/sheets/demon-sheet.js
@@ -1,19 +1,24 @@
-export default class DemonSheet  extends ItemSheet {
-	static get defaultOptions() {
-	    return(foundry.utils.mergeObject(super.defaultOptions,
-	    	                             {classes: ["bsh", "bsh-sheet", "bsh-demon"],
-	    	                              height: 350,
-	    	               	              template: "systems/black-sword-hack/templates/sheets/demon-sheet.html",
-	    	                              width: 700}));
-	}
+export default class DemonSheet extends ItemSheet {
 
-	get template() {
-		return("systems/black-sword-hack/templates/sheets/demon-sheet.html");
-	}
+    static get defaultOptions() {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+            classes:  ["bsh", "bsh-sheet", "bsh-demon", "sheet"],
+            height:   350,
+            width:    700,
+            template: "systems/black-sword-hack/templates/sheets/demon-sheet.html"
+        });
+    }
 
-	getData() {
-		let data = super.getData();
-		data.configuration = CONFIG.configuration;
-		return(data);
-	}
+    get template() {
+        return "systems/black-sword-hack/templates/sheets/demon-sheet.html";
+    }
+
+    // V14 : getData() retourne item et item.system directement
+    getData() {
+        const context         = super.getData();
+        context.configuration = CONFIG.configuration;
+        context.item          = this.item;
+        context.system        = this.item.system;
+        return context;
+    }
 }
