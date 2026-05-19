f14c0a364757b9760e5bd784e3eb06de0433fc37
Bynouz <74014467+Bynouz@users.noreply.github.com>
2026-05-19 20:33:33 +0200

    Update spell-sheet.js

--- modules/sheets/spell-sheet.js
+++ modules/sheets/spell-sheet.js
@@ -1,12 +1,25 @@
-export default class SpellSheet  extends ItemSheet {
-	get template() {
-		return("systems/black-sword-hack/templates/sheets/spell-sheet.html");
-	}
+export default class SpellSheet extends ItemSheet {
 
-	getData() {
-		let data = super.getData();
-		data.configuration = CONFIG.configuration;
-		data.disabled      = (data.item.system.state === "unavailable");
-		return(data);
-	}
+    static get defaultOptions() {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+            classes:  ["bsh", "bsh-sheet", "bsh-spell-sheet", "sheet"],
+            height:   400,
+            width:    550,
+            template: "systems/black-sword-hack/templates/sheets/spell-sheet.html"
+        });
+    }
+
+    get template() {
+        return "systems/black-sword-hack/templates/sheets/spell-sheet.html";
+    }
+
+    // V14 : getData() retourne item et item.system directement
+    getData() {
+        const context         = super.getData();
+        context.configuration = CONFIG.configuration;
+        context.item          = this.item;
+        context.system        = this.item.system;
+        context.disabled      = (this.item.system.state === "unavailable");
+        return context;
+    }
 }
